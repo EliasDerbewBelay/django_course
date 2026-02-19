a7b8df8e7d3bea291a760041bc493056ef9f5932
eliasderbewbelay <eliasderbew1@gmail.com>
2026-02-19 22:55:57 +0300

    add zip into the address models

--- store/models.py
+++ store/models.py
@@ -61,7 +61,9 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length = 255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
