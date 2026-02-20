2a29b879c1ef35a8826464b555ecb43decbe07bb
eliasderbewbelay <eliasderbew1@gmail.com>
2026-02-20 15:50:35 +0300

    connect the mysql database with the django

--- backend/settings.py
+++ backend/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storfront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': "Qwerty123%&*",
+
     }
 }
 
